f2eee60c58b6d1577a1f67d1ddf38c5e6aac9d57
getcaas <contact@getcaas.io>
2023-02-27 14:53:52 +0100

    add img preview

--- src/containers/NewImage.js
+++ src/containers/NewImage.js
@@ -1,28 +1,51 @@
-import React, { useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Form from "react-bootstrap/Form";
-import { API } from "aws-amplify";
-import { useNavigate } from "react-router-dom";
+import {API} from "aws-amplify";
+import {useNavigate} from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
-import { onError } from "../lib/errorLib";
+import {onError} from "../lib/errorLib";
 import config from "../config";
 import {s3Upload} from "../lib/awsLib";
+
 export default function NewImage() {
     const file = useRef(null);
     const nav = useNavigate();
     const [content, setContent] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [selectedFile, setSelectedFile] = useState();
+    const [preview, setPreview] = useState();
+
+    useEffect(() => {
+        if (!selectedFile) {
+            setPreview(undefined)
+            return
+        }
+
+        const objectUrl = URL.createObjectURL(selectedFile)
+        setPreview(objectUrl)
+
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    }, [selectedFile])
 
     function validateForm() {
         return content.length > 0;
     }
 
-    function handleFileChange(event) {
-        file.current = event.target.files[0];
+    function handleFileChange(e) {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(undefined)
+            return
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        setSelectedFile(e.target.files[0])
+
+        file.current = e.target.files[0];
     }
 
     async function handleSubmit(event) {
         event.preventDefault();
-
         if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
             alert(
                 `Please pick a file smaller than ${
@@ -37,7 +60,7 @@ export default function NewImage() {
         try {
             const attachment = file.current ? await s3Upload(file.current) : null;
 
-            await createImage({ content, attachment });
+            await createImage({content, attachment});
             nav("/");
         } catch (e) {
             onError(e);
@@ -45,11 +68,11 @@ export default function NewImage() {
         }
     }
 
-        function createImage(data) {
-            return API.post("images", "/images", {
-                body: data,
-            });
-        }
+    function createImage(data) {
+        return API.post("images", "/images", {
+            body: data,
+        });
+    }
 
     return (
         <div className="NewNote">
@@ -63,9 +86,17 @@ export default function NewImage() {
                     />
                 </Form.Group>
                 <Form.Group controlId="file">
-                    <Form.Label>Attachment</Form.Label>
-                    <Form.Control onChange={handleFileChange} type="file" />
+                    <Form.Label>Attachment (.png, .jpg, .jpeg )</Form.Label>
+                    <Form.Control accept='.png, .jpg, .jpeg' onChange={handleFileChange} type="file"/>
                 </Form.Group>
+                {selectedFile &&
+                    <Form.Group>
+                        <Form.Label>Preview</Form.Label>
+                        <div>
+                            <img src={preview} alt="preview" height={200}/>
+                        </div>
+                    </Form.Group>
+                }
                 <LoaderButton
                     block
                     type="submit"
